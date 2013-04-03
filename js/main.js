fa86091a3013d42df0beae4e2c366b4215d7a8b0
Pavel Vinnyk <pavel@ecomarkup.com>
2013-04-03 10:22:36 +0300

    no message

--- js/main.js
+++ js/main.js
@@ -6,10 +6,16 @@ jQuery(function ($) {
     $('.search-body__i input[type="text"]').blur(function () {
         $('.popup-text').fadeIn(200);
     });
-
-    $(window).load(function () {
-       
-                 
-    });
+     $(".gallery__item a").hover(function () {
+     	var galleryItem = $(this).parent();
+     	if(galleryItem.width()<140){
+       		galleryItem.find(".hover-box").css({
+       			"min-width":"120px",
+       			"margin-left":"-74px",
+       			"left":"50%"
+       		});
+    	}
+	});
+    
         
 }); 
